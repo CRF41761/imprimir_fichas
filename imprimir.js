53cf84db5b17d992eacd38980b78e42f92c25cae
CRF41761 <crf.granja.saler@gmail.com>
2025-12-10 10:16:07 +0100

    Update imprimir.js

--- imprimir.js
+++ imprimir.js
@@ -1,5 +1,5 @@
 // ✅ URL de tu Google Sheets (Web App de Apps Script)
-const SPREADSHEET_URL = "https://script.google.com/macros/s/AKfycbyYcS_Tj5kzBuh3-2AutGLMq96eNqkyy-iMPLJCFWk1Gl8f_uyX7Tb5MBwqjJ9N0YnF/exec"; // <-- Asegúrate de que esta URL sea la NUEVA que publicaste
+const SPREADSHEET_URL = "https://script.google.com/macros/s/AKfycbzaMvCNFMQUI0g6SpU-VVkV66UPTvxeIRjbELiGFvS4qfoEttB0qbbQ3ID6AYPv0E3s/exec"; // <-- Asegúrate de que esta URL sea la NUEVA que publicaste
 
 /* -------------------------
    Helper JSONP (evita CORS)
@@ -212,6 +212,7 @@ document.getElementById('btnBuscar')?.addEventListener('click', () => {
 document.getElementById('buscador')?.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') buscarFichas(e.target.value);
 });
+
 
 
 
