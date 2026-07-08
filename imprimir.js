6a30cd6523aa7a1dc7de6cbad547f8b590985e34
CRF41761 <crf.granja.saler@gmail.com>
2026-07-08 13:29:47 +0200

    Update imprimir.js

--- imprimir.js
+++ imprimir.js
@@ -92,10 +92,10 @@ async function buscarFichas() {
         if (fecha) params.append('fecha', fecha);
         if (cumplimentado) params.append('cumplimentado', cumplimentado);
         
-        // Si no hay filtros, cargar últimas 200
-        if (!numero && !especie && !municipio && !fecha && !cumplimentado) {
-            params.append('limit', '200');
-        }
+        // Si no hay filtros, cargar últimas 50
+if (!numero && !especie && !municipio && !fecha && !cumplimentado) {
+    params.append('limit', '50');
+}
         
         const data = await loadJSONP(`${SPREADSHEET_URL}?${params.toString()}`);
         if (!Array.isArray(data)) {
